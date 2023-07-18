4766c02226a62bce1999f8883198a356c9032dc1
annKlush <109134951+annKlush@users.noreply.github.com>
2023-07-18 10:25:21 +0300

    com

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,28 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> developerProjects = new ArrayList<>();
+        for (String project : projects.keySet()) {
+            Set<String> developers = projects.get(project);
+            if (developers.contains(developer)) {
+                developerProjects.add(project);
+            }
+        }
+
+        Collections.sort(developerProjects, new Comparator<String>() {
+            public int compare(String project1, String project2) {
+                int lengthComparison = Integer.compare(project2.length(), project1.length());
+                if (lengthComparison != 0) {
+                    return lengthComparison;
+                } else {
+                    return project2.compareTo(project1);
+                }
+            }
+        });
+
+        return developerProjects;
     }
 }
